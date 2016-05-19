36f8a6b0668a87186b060b9d85f752e66792dd2a
Kristen Kanekuni <kxk9106@g.rit.edu>
2016-05-19 09:36:21 -0400

    Exam Practice Stuff Done

--- Ex02_C/MyBOClass.cpp
+++ Ex02_C/MyBOClass.cpp
@@ -152,6 +152,38 @@ MyBOClass::MyBOClass(std::vector<vector3> a_lVectorList)
 		return;
 
 	//Your code goes Here
+	//Max and min as the first vector of the list
+	m_v3Max = m_v3Min = a_lVectorList[0];
+
+	//Get the max and min out of the list
+	for (uint nVertex = 1; nVertex < nVertexCount; nVertex++)
+	{
+		if (m_v3Min.x > a_lVectorList[nVertex].x) //If min is larger than current
+			m_v3Min.x = a_lVectorList[nVertex].x;
+		else if (m_v3Max.x < a_lVectorList[nVertex].x)//if max is smaller than current
+			m_v3Max.x = a_lVectorList[nVertex].x;
+
+		if (m_v3Min.y > a_lVectorList[nVertex].y) //If min is larger than current
+			m_v3Min.y = a_lVectorList[nVertex].y;
+		else if (m_v3Max.y < a_lVectorList[nVertex].y)//if max is smaller than current
+			m_v3Max.y = a_lVectorList[nVertex].y;
+
+		if (m_v3Min.z > a_lVectorList[nVertex].z) //If min is larger than current
+			m_v3Min.z = a_lVectorList[nVertex].z;
+		else if (m_v3Max.z < a_lVectorList[nVertex].z)//if max is smaller than current
+			m_v3Max.z = a_lVectorList[nVertex].z;
+	}
+
+	//with the max and the min we calculate the center
+	m_v3Center = (m_v3Min + m_v3Max) / 2.0f;
+
+	//we calculate the distance between all the values of min and max vectors
+	m_v3HalfWidth = (m_v3Max - m_v3Min) / 2.0f;
+
+	m_v3MaxG = m_v3Max;
+	m_v3MinG = m_v3Min;
+	m_v3CenterG = m_v3Center;
+	m_v3HalfWidthG = m_v3HalfWidth;
 }
 void MyBOClass::SetModelMatrix(matrix4 a_m4ToWorld)
 {
@@ -162,4 +194,44 @@ void MyBOClass::SetModelMatrix(matrix4 a_m4ToWorld)
 
 	m_m4ToWorld = a_m4ToWorld;
 	//Your code goes here
+	//Calculate the vertex that makes the box
+	vector3 v3Corner[8];
+	v3Corner[0] = vector3(m_v3Min.x, m_v3Min.y, m_v3Min.z);
+	v3Corner[1] = vector3(m_v3Max.x, m_v3Min.y, m_v3Min.z);
+	v3Corner[2] = vector3(m_v3Min.x, m_v3Max.y, m_v3Min.z);
+	v3Corner[3] = vector3(m_v3Max.x, m_v3Max.y, m_v3Min.z);
+
+	v3Corner[4] = vector3(m_v3Min.x, m_v3Min.y, m_v3Max.z);
+	v3Corner[5] = vector3(m_v3Max.x, m_v3Min.y, m_v3Max.z);
+	v3Corner[6] = vector3(m_v3Min.x, m_v3Max.y, m_v3Max.z);
+	v3Corner[7] = vector3(m_v3Max.x, m_v3Max.y, m_v3Max.z);
+	//Get vectors in global space
+	for (uint nVertex = 0; nVertex < 8; nVertex++)
+	{
+		v3Corner[nVertex] = vector3(m_m4ToWorld * vector4(v3Corner[nVertex], 1.0f));
+	}
+
+	//Get the max and min out of the list
+	m_v3MaxG = m_v3MinG = v3Corner[0];
+	for (uint nVertex = 1; nVertex < 8; nVertex++)
+	{
+		if (m_v3MinG.x > v3Corner[nVertex].x) //If min is larger than current
+			m_v3MinG.x = v3Corner[nVertex].x;
+		else if (m_v3MaxG.x < v3Corner[nVertex].x)//if max is smaller than current
+			m_v3MaxG.x = v3Corner[nVertex].x;
+
+		if (m_v3MinG.y > v3Corner[nVertex].y) //If min is larger than current
+			m_v3MinG.y = v3Corner[nVertex].y;
+		else if (m_v3MaxG.y < v3Corner[nVertex].y)//if max is smaller than current
+			m_v3MaxG.y = v3Corner[nVertex].y;
+
+		if (m_v3MinG.z > v3Corner[nVertex].z) //If min is larger than current
+			m_v3MinG.z = v3Corner[nVertex].z;
+		else if (m_v3MaxG.z < v3Corner[nVertex].z)//if max is smaller than current
+			m_v3MaxG.z = v3Corner[nVertex].z;
+	}
+	m_v3CenterG = (m_v3MinG + m_v3MaxG) / 2.0f;
+
+	//we calculate the distance between all the values of min and max vectors
+	m_v3HalfWidthG = (m_v3MaxG - m_v3MinG) / 2.0f;
 }
